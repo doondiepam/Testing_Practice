b07f386d49cf47fc4c12a6e5ab739305ec1940f9
Karicherla Doondi Durga Vivek <karicherla_doondidurgavivek@epam.com>
2026-02-19 11:34:29 +0530

    19-02-2026

---
+++ selenium_testing/Sample_testing.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+
+from selenium.webdriver.chrome.service import Service
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://www.google.com")
+
+time.sleep(5)
+
+driver.quit()
+
+
